64fa84626eb6fc69e734e357f15b44f216d39207
zhtroy <zhtroy@sina.com>
2019-05-09 15:00:58 +0800

    修改最大油门140 安全距离20~40m

--- Source/Thread/Moto/task_moto.h
+++ Source/Thread/Moto/task_moto.h
@@ -199,7 +199,7 @@ typedef struct{
 #define MAX_THROTTLE_SIZE (100)
 
 #elif CAR_VERSION == 22
-#define MAX_THROTTLE_SIZE (110)
+#define MAX_THROTTLE_SIZE (140)
 
 #endif
 
@@ -218,8 +218,8 @@ typedef struct{
 /*
  * 安全距离范围定义,单位(10cm)
  */
-#define MIN_SAFE_DISTANCE (50)
-#define MAX_SAFE_DISTANCE (200)
+#define MIN_SAFE_DISTANCE (200)
+#define MAX_SAFE_DISTANCE (400)
 /*
  * 远离距离范围定义,单位(10cm)
  */
