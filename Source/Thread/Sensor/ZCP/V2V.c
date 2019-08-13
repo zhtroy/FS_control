a9fcb120afdefba46cb4477a3e03d461eef7aa7f
zhtroy <zhtroy@sina.com>
2019-08-13 18:52:54 +0800

    数据不同步，临时取消安全距离

--- Source/Thread/Sensor/ZCP/V2V.c
+++ Source/Thread/Sensor/ZCP/V2V.c
@@ -214,14 +214,7 @@ static void V2VSendTask(UArg arg0, UArg arg1)
 
 		}
 
-		if(myepc.areaType != EPC_AREATYPE_STATION && isSameAdjustArea == 0 && m_distanceToFrontCar < DANGER_DISTANCE
-				&& MotoGetCarMode() == Auto)
-		{
-		    /*
-		     * 自动模式，非调整区，非站台区，安全距离太近
-		     */
-		    Message_postError(ERROR_SAFE_DISTANCE);
-		}
+
 
 		g_fbData.forwardCarDistance = m_distanceToFrontCar;
 		g_fbData.forwardCarRPM = m_frontCarStatus.rpm;
