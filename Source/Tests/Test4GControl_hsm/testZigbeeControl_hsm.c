5455ce723ac3ce4017aa99832953cf7f036c5eae
zhtroy <zhtroy@sina.com>
2019-08-14 08:53:54 +0800

    心跳改为10s

--- Source/Tests/Test4GControl_hsm/testZigbeeControl_hsm.c
+++ Source/Tests/Test4GControl_hsm/testZigbeeControl_hsm.c
@@ -54,6 +54,8 @@
 #define REMOTE_CMD_KAP           20
 #define REMOTE_CMD_KAI           21
 
+//zigbee遥控超时
+#define ZIGBEE_TIMEOUT (10000)
 
 
 extern Void taskCreateRFID(UArg a0, UArg a1);
@@ -100,7 +102,7 @@ static Void taskZigbeeControlMain_hsm(UArg a0, UArg a1)
 	Clock_Params_init(&clockParams);
 	clockParams.period = 0;       // one shot
 	clockParams.startFlag = FALSE;
-	heartClock = Clock_create(connectionClosed, 3000, &clockParams, &eb); //3s 后没有收到包就停止
+	heartClock = Clock_create(connectionClosed, ZIGBEE_TIMEOUT, &clockParams, &eb); //没有收到包就停止
 	if ( heartClock == NULL )
 	{
 		System_abort("Clock create failed\n");
