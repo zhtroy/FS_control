56256bc931b45def4d3b5f2bf774dae65bc5297d
dongfang219@126.com <dong127fang219>
2019-11-19 18:43:31 +0800

    修改脚本，同时生成rprc和ais

--- out/copy_genrprc.bat
+++ out/copy_genrprc.bat
@@ -1,3 +1,4 @@
 cd /d %cd%\..\out
 copy %cd%\..\Debug\fs_control.out %cd%\fs_control.out
 out2rprc.exe fs_control.out fs_control.bin
+HexAIS_OMAP-L138.exe -ini NandFlash.ini -o fs_control.ais fs_control.out
